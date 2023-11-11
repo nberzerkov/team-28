897500312d9bac5cfe72efa3c72a3d0cbff0c477
enduraew <enduraew@gmail.com>
2023-11-11 16:34:59 +0500

    Feat: import flip module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 'use strict';
 import 'normalize.css';
+
+import flipCard from './js/flip-card';
+
 if (process.env.NODE_ENV !== 'production') {
    console.log('Development mode');
 }
+
+flipCard();
