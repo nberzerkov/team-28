67901b79e52094a6d5ec860a667538bde9bbf20c
enduraew <enduraew@gmail.com>
2023-11-11 17:19:28 +0500

    Implement shuffle-card logic

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@
 import 'normalize.css';
 
 import flipCard from './js/flip-card';
+import shuffleCards from './js/shuffleCards';
 
 if (process.env.NODE_ENV !== 'production') {
    console.log('Development mode');
 }
 
 flipCard();
+shuffleCards();
---
+++ src/js/shuffleCards.js
@@ -0,0 +1,5 @@
+export default function shuffleCards() {
+   const gameBoard = document.querySelector('.game__grid');
+   const cards = [...gameBoard.children];
+   console.log(cards);
+}
