edf5b8bcfa7a1a903dca8ae202596940cfb89b2b
Maria <mi.kovalenko@mail.ru>
2023-11-11 15:39:52 +0400

    .

--- src/index.js
+++ src/index.js
@@ -3,3 +3,14 @@ import 'normalize.css';
 if (process.env.NODE_ENV !== 'production') {
    console.log('Development mode');
 }
+
+let countValue
+
+function countMove() {
+  const count = document.getElementById('count')
+  if (count) {
+    countValue = parseInt(count.innerHTML)
+    countValue++;
+  }  
+}
+
