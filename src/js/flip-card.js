bc81d73cd6034e674a154362e260ea7a9c34dbc8
enduraew <enduraew@gmail.com>
2023-11-11 22:10:49 +0500

    Implement match card function

--- src/js/flip-card.js
+++ src/js/flip-card.js
@@ -1,9 +1,29 @@
-export default function flipCard() {
-   const gameCards = document.querySelectorAll('.game__card');
+import checkForMatch from './isMatchesCards';
 
+const gameCards = document.querySelectorAll('.game__card');
+
+export let firstSelectedCard;
+export let secondSelectedCard;
+let isCardFlipped = false;
+
+export function flipCards() {
+   this.classList.toggle('is-flipped');
+   if (!isCardFlipped) {
+      isCardFlipped = true;
+      firstSelectedCard = this;
+      return;
+   }
+
+   secondSelectedCard = this;
+   isCardFlipped = false;
+   console.log(firstSelectedCard);
+   console.log(secondSelectedCard);
+
+   checkForMatch();
+}
+
+export default function flipCard() {
    gameCards.forEach(cards => {
-      cards.addEventListener('click', e => {
-         cards.classList.toggle('is-flipped');
-      });
+      cards.addEventListener('click', flipCards);
    });
 }
--- src/js/isMatchesCards.js
+++ src/js/isMatchesCards.js
@@ -1,1 +1,16 @@
-export default function matchCards() {}
+import { firstSelectedCard, secondSelectedCard } from './flip-card';
+import freezeCards from './freezeCards';
+
+export default function checkForMatch() {
+   if (firstSelectedCard.getAttribute('data-id') === secondSelectedCard.getAttribute('data-id')) {
+      console.log('freeze cards');
+      return;
+   }
+
+   setTimeout(() => {
+      firstSelectedCard.classList.remove('is-flipped');
+      secondSelectedCard.classList.remove('is-flipped');
+   }, '800');
+
+   console.log('Unflip cards');
+}
