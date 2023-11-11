b0399555eaa3fbb77fabd79a4d243e403fb0c3e5
enduraew <enduraew@gmail.com>
2023-11-11 17:51:08 +0500

    Feat: shuffle card implementation

--- src/js/shuffleCards.js
+++ src/js/shuffleCards.js
@@ -1,5 +1,9 @@
 export default function shuffleCards() {
    const gameBoard = document.querySelector('.game__grid');
    const cards = [...gameBoard.children];
-   console.log(cards);
+   // console.log(cards);
+   cards.forEach(card => card.remove());
+   const randomCards = cards.map(child => ({ child, rand: Math.random() }));
+   randomCards.sort((a, b) => b.rand - a.rand);
+   randomCards.forEach(randomObj => gameBoard.appendChild(randomObj.child));
 }
