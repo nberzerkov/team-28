ef2753529f749464242ffda9862e1e2225416c89
enduraew <enduraew@gmail.com>
2023-11-12 10:28:58 +0500

    Fix typo

--- src/js/isMatchesCards.js
+++ src/js/isMatchesCards.js
@@ -5,7 +5,6 @@ export let countFreeze = 0;
 export default function checkForMatch() {
    if (firstSelectedCard.getAttribute('data-id') === secondSelectedCard.getAttribute('data-id')) {
       // здесь надо написать функционал чтобы нельзя было нажать на фриз карточку
-      console.log('freeze cards');
       ++countFreeze;
       console.log(countFreeze);
       return;
