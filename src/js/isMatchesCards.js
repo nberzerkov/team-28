2c6e5a920e22cf231593061eec0b09bae289406c
enduraew <enduraew@gmail.com>
2023-11-12 10:17:40 +0500

    Remove: console.logs

--- src/js/isMatchesCards.js
+++ src/js/isMatchesCards.js
@@ -3,7 +3,6 @@ import { firstSelectedCard, secondSelectedCard } from './flip-card';
 export default function checkForMatch() {
    if (firstSelectedCard.getAttribute('data-id') === secondSelectedCard.getAttribute('data-id')) {
       // здесь надо написать функционал чтобы нельзя было нажать на фриз карточку
-      console.log('freeze cards');
       return;
    }
 
@@ -11,6 +10,4 @@ export default function checkForMatch() {
       firstSelectedCard.classList.remove('is-flipped');
       secondSelectedCard.classList.remove('is-flipped');
    }, '800');
-
-   console.log('Unflip cards');
 }
