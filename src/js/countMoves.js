ab6957600059cabeb38aadc3b1ac579ad9858a3d
enduraew <enduraew@gmail.com>
2023-11-12 14:50:31 +0500

    Fix

--- src/js/countMoves.js
+++ src/js/countMoves.js
@@ -1,20 +1,10 @@
-import { countFreeze } from "./isMatchesCards";
-
 let countValue = 0;
 
 export default function countMoves() {
-  if (countFreeze < 1) {
    const count = document.getElementById('count');
    if (count) {
-    ++countValue;
-    count.textContent = countValue;
-    console.log(countValue);
+      ++countValue;
+      count.textContent = countValue;
+      console.log(countValue);
    }
-  } else {
-    const modal = document.getElementById('modal')
-    if (modal) {
-      console.log(modal)
-      modal.classList.add('modal--open') //почему-то не работает, но вроде должно?
-    }
-  }
 }
